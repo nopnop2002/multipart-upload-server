c1ce040927c46b11257bdb0c597a3d38213d6843
Nishikawa <hau26280@tree.odn.ne.jp>
2021-05-22 14:59:36 +0900

    Update flask

--- flask/upload.py
+++ flask/upload.py
@@ -1,32 +1,127 @@
 # -*- coding: utf-8 -*-
 import os
+import sys
 import werkzeug
 from datetime import datetime
-from flask import Flask, request, make_response, jsonify
+from flask import Flask, request, render_template, send_file, Blueprint
 from werkzeug.serving import WSGIRequestHandler
 import logging
 import json
+import magic
+import urllib
 
 # flask
 app = Flask(__name__)
 logging.basicConfig(level=logging.INFO)
 
-
-# ex) set UPLOAD_DIR_PATH=C:/tmp/flaskUploadDir
-UPLOAD_DIR = os.getenv("UPLOAD_DIR_PATH")
+# create UPLOAD_DIR
+UPLOAD_DIR = os.path.join(os.getcwd(), "uploaded")
 logging.info("UPLOAD_DIR={}".format(UPLOAD_DIR))
-if (UPLOAD_DIR == None):
-    UPLOAD_DIR = os.path.join(os.getcwd(), "UPLOADED")
-    logging.info("UPLOAD_DIR={}".format(UPLOAD_DIR))
-
 if (os.path.exists(UPLOAD_DIR) == False):
-    logging.warning("UPLOAD_DIR [{}] not found. Create this".format(UPLOAD_DIR))
-    os.mkdir(UPLOAD_DIR)
+	logging.warning("UPLOAD_DIR [{}] not found. Create this".format(UPLOAD_DIR))
+	os.mkdir(UPLOAD_DIR)
+
+# Added /uploaded to static_url_path
+add_app = Blueprint("uploaded", __name__, static_url_path='/uploaded', static_folder='./uploaded')
+app.register_blueprint(add_app)
+
 
 @app.route("/")
 def hello():
-	print("hello")
-	return "Hello World!"
+	files = []
+	dirs = []
+	meta = {
+		"current_directory": UPLOAD_DIR
+	}
+
+	for (dirpath, dirnames, filenames) in os.walk(UPLOAD_DIR):
+		logging.info("filenames={}".format(filenames))
+		for name in filenames:
+			nm = os.path.join(dirpath, name).replace(UPLOAD_DIR, "").strip("/").split("/")
+			logging.info("nm={}".format(nm))
+			# Skip if the file is in a subdirect
+			# nm=['templates', 'index.html']
+			if len(nm) != 1: continue
+
+			fullpath = os.path.join(dirpath, name)
+			logging.info("fullpath={}".format(fullpath))
+			if os.path.isfile(fullpath) == False: continue
+			size = os.stat(fullpath).st_size
+
+			mime = magic.from_file(fullpath, mime=True)
+			logging.info("mime={}".format(mime))
+			visible = "image/" in mime
+			if (visible == False):
+				visible = "text/" in mime
+			logging.info("visible={}".format(visible))
+
+			if sys.version_info.major == 2:
+				files.append({
+					"name": name,
+					"size": str(size) + " B",
+					"mime": mime,
+					"fullname": urllib.quote_plus(fullpath),
+					"visible": visible
+				})
+
+			if sys.version_info.major == 3:
+				files.append({
+					"name": name,
+					"size": str(size) + " B",
+					"mime": mime,
+					"fullname": urllib.parse.quote_plus(fullpath),
+					"visible": visible
+				})
+
+
+	return render_template("index.html", files=sorted(files, key=lambda k: k["name"].lower()), folders=dirs, meta=meta)
+
+
+@app.route("/download/<filename>")
+def download(filename):
+	logging.info("{}:filename={}".format(sys._getframe().f_code.co_name, filename))
+	if sys.version_info.major == 2:
+		filename = urllib.unquote(filename)
+	if sys.version_info.major == 3:
+		filename = urllib.parse.unquote(filename)
+
+	if os.path.isfile(filename):
+		if os.path.dirname(filename) == UPLOAD_DIR.rstrip("/"):
+			return send_file(filename, as_attachment=True)
+		else:
+			return render_template("no_permission.html")
+	else:
+		return render_template("not_found.html")
+	return None
+
+@app.route("/imageview/<filename>")
+def imageview(filename):
+	logging.info("{}:filename={}".format(sys._getframe().f_code.co_name, filename))
+	if sys.version_info.major == 2:
+		filename = urllib.unquote(filename)
+	if sys.version_info.major == 3:
+		filename = urllib.parse.unquote(filename)
+
+	mime = magic.from_file(filename, mime=True)
+	mime = mime.split("/")
+	logging.info("mime={}".format(mime))
+
+	if (mime[0] == "image"):
+		logging.debug("filename={}".format(filename))
+		filename = os.path.basename(filename)
+		logging.debug("filename={}".format(filename))
+		filename = os.path.join("/uploaded", filename)
+		logging.debug("filename={}".format(filename))
+		return render_template("view.html", user_image = filename)
+
+	if (mime[0] == "text"):
+		contents = ""
+		f = open(filename, 'r')
+		datalist = f.readlines()
+		for data in datalist:
+			print("[{}]".format(data.rstrip()))
+			contents = contents + data.rstrip() + "<br>"
+		return contents
 
 @app.route('/upload_multipart', methods=['POST'])
 def upload_multipart():
@@ -69,7 +164,6 @@ def upload_multipart():
 	except IOError as e:
 		logging.error("Failed to write file due to IOError %s", str(e))
 		responce = {'result':'upload FAIL'}
-
 
 	return json.dumps(responce)
 
